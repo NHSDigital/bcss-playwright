ac9545dea36dce78cdd49043ebd1facd34f0d029
Ihsanul Mirza <ihsanul.mirza@ibm.com>
2025-02-27 13:42:17 +0000

    added page object model for contacts list page

---
+++ pages/contacts_list_page.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import Page
+
+
+class ContactsListPage:
+    def __init__(self, page: Page):
+        self.page = page
+        # ContactsList Page
+        self.view_contacts_page = self.page.get_by_role("link", name="View Contacts")
+        self.edit_my_contact_details_page = self.page.get_by_role("link", name="Edit My Contact Details")
+        self.maintain_contacts_page = self.page.get_by_role("link", name="Maintain Contacts")
+        self.my_preference_settings_page = self.page.get_by_role("link", name="My Preference Settings")
+
+
+    def go_to_view_contacts_page(self):
+        self.view_contacts_page.click()
+
+    def go_to_edit_my_contact_details_page(self):
+        self.edit_my_contact_details_page.click()
+
+    def go_to_maintain_contacts_details_page(self):
+        self.maintain_contacts_page.click()
+
+    def go_to_my_preference_settings_page(self):
+        self.my_preference_settings_page.click()
+
+
