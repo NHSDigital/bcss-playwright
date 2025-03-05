4e32221b20ba5a962dca9dfc602f1f7bac4e7833
Victor Soares <victor.soares-cic@ibm.com>
2025-03-05 13:18:50 +0000

    Added a method to add users to the UI Approved Users table

--- utils/oracle.py
+++ utils/oracle.py
@@ -26,3 +26,35 @@ def database_connection_query(sql: str): # To use when "select a from b"
             for r in cursor.execute(sql):
                 print(r)
             return r
+
+def populate_ui_approved_users_table(user: str): # To add users to the UI_APPROVED_USERS table
+    load_dotenv()
+    un = os.getenv("un")
+    cs = os.getenv("cs")
+    pw = os.getenv("pw")
+
+    try:
+        print("Attempting DB connection...")
+        conn = oracledb.connect(user=un, password=pw, dsn=cs)
+        print(conn.version, "DB connection successful!")
+        try:
+            print("Attempting to write to the db...")
+            cursor = conn.cursor()
+            cursor.execute(f'INSERT INTO "UI_APPROVED_USERSf" (OE_USER_CODE) VALUES ({user})')
+            print(conn.version, "DB write successful!")
+        except Exception as connectionError:
+            print(f"Failed to connect to the DB! with connection error {connectionError}")
+        finally:
+            if conn is not None:
+                conn.close()
+    except Exception as dbWriteError:
+        print(f"Failed to write to the DB! with write error {dbWriteError}")
+
+
+
+
+
+
+
+
+
