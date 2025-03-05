c2b00ce4e6888ecbd0a697712dc77b47ccc6f6c5
Adriano Aru <adriano.aru1@nhs.net>
2025-03-05 17:08:13 +0000

    Altering existing functions to include try:

--- utils/oracle.py
+++ utils/oracle.py
@@ -2,30 +2,51 @@ import oracledb
 import os
 from dotenv import load_dotenv
 
-# Edit to "try:"
-
 def database_connection_exec(procedure: str): # To use when "exec xxxx" (stored procedures)
     load_dotenv()
     un = os.getenv("un")
     cs = os.getenv("cs")
     pw = os.getenv("pw")
 
-    with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
-        with connection.cursor() as cursor:
-            for r in cursor.callproc(procedure):
-                print(r)
+    try:
+        print("Attempting DB connection...")
+        conn = oracledb.connect(user=un, password=pw, dsn=cs)
+        print(conn.version, "DB connection successful!")
+        try:
+            print(f"Attempting to execute stored procedure: {procedure}")
+            cursor = conn.cursor()
+            cursor.callproc(procedure)
+            print(conn.version, "DB write successful!")
+        except Exception as executionError:
+            print(f"Failed to execute stored procedure with execution error: {executionError}")
+        finally:
+            if conn is not None:
+                conn.close()
+    except Exception as connectionError:
+            print(f"Failed to connect to the DB! with connection error: {connectionError}")
 
-def database_connection_query(sql: str): # To use when "select a from b"
+def database_connection_query(query: str): # To use when "exec xxxx" (stored procedures)
     load_dotenv()
     un = os.getenv("un")
     cs = os.getenv("cs")
     pw = os.getenv("pw")
 
-    with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
-        with connection.cursor() as cursor:
-            for r in cursor.execute(sql):
-                print(r)
-            return r
+    try:
+        print("Attempting DB connection...")
+        conn = oracledb.connect(user=un, password=pw, dsn=cs)
+        print(conn.version, "DB connection successful!")
+        try:
+            print(f"Attempting to execute query: {query}")
+            cursor = conn.cursor()
+            cursor.execute(query)
+            print(conn.version, "DB write successful!")
+        except Exception as executionError:
+            print(f"Failed to execute query with execution error {executionError}")
+        finally:
+            if conn is not None:
+                conn.close()
+    except Exception as connectionError:
+            print(f"Failed to connect to the DB! with connection error {connectionError}")
 
 def populate_ui_approved_users_table(user: str): # To add users to the UI_APPROVED_USERS table
     load_dotenv()
@@ -42,14 +63,13 @@ def populate_ui_approved_users_table(user: str): # To add users to the UI_APPROV
             cursor = conn.cursor()
             cursor.execute(f'INSERT INTO "UI_APPROVED_USERSf" (OE_USER_CODE) VALUES ({user})')
             print(conn.version, "DB write successful!")
-        except Exception as connectionError:
-            print(f"Failed to connect to the DB! with connection error {connectionError}")
+        except Exception as dbWriteError:
+            print(f"Failed to write to the DB! with write error {dbWriteError}")
         finally:
             if conn is not None:
                 conn.close()
-    except Exception as dbWriteError:
-        print(f"Failed to write to the DB! with write error {dbWriteError}")
-
+    except Exception as connectionError:
+            print(f"Failed to connect to the DB! with connection error {connectionError}")
 
 
 
