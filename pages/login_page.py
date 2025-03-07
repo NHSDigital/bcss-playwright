fba58dce63e74bfe77c7cd9113671df4a1e8860e
AndyG <179915678+Andyg79@users.noreply.github.com>
2025-03-07 09:43:58 +0000

    Added .env file to project Removed negative login tests Removed redundant login method from BcssLoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,6 +4,7 @@ from playwright.sync_api import Page
 from utils.user_tools import UserTools
 from dotenv import load_dotenv
 
+
 class BcssLoginPage:
 
     def __init__(self, page: Page):
@@ -12,17 +13,6 @@ class BcssLoginPage:
         self.username = page.get_by_role("textbox", name="Username")
         self.password = page.get_by_role("textbox", name="Password")
         self.submit_button = page.get_by_role("button", name="submit")
-
-
-    def login(self, username, password):
-        """Logs in to bcss with specified user credentials
-        Args:
-            username (str) enter a username
-            password (str) enter a password
-        """
-        self.username.fill(username)
-        self.password.fill(password)
-        self.submit_button.click()
 
     def login_as_user_bcss401(self):
         """Logs in to bcss as the test user 'BCSS401'"""
