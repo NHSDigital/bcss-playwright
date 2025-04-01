5d841611938c3e61d76cac15b3cec498c6041daa
Adriano Aru <adriano.aru1@nhs.net>
2025-04-01 12:47:20 +0100

    Docstrings added for utils/fit_kit_generation and utils/oracle/oracle_specific_functions

--- utils/fit_kit_generation.py
+++ utils/fit_kit_generation.py
@@ -5,12 +5,24 @@ import logging
 
 
 def create_fit_id_df() -> pd.DataFrame:
+    """
+    The first step here is to get the relevant test data for compartment 2
+    Then it calculates the check digit for each kit id retrieved
+    Finally it adds the final part on the end (expiry date + random characters)
+    """
     df = get_kit_id_from_db()
     df["fit_device_id"] = df["kitid"].apply(calculate_check_digit)
     df["fit_device_id"] = df["fit_device_id"].apply(convert_kit_id_to_fit_device_id)
     return df
 
 def calculate_check_digit(kit_id: str) -> str:
+    """
+    This function used used to calculate the check digit of a kit ID
+    It calculates the check digit by getting the sum of the location of each character in the kit id
+    Then it divides the sum by 43 and gets the remainder from this
+    It then searches the string "char_string" to find the index of the remainder
+    The character found is then the check digit
+    """
     logging.info(f"Calculating check digit for kit id: {kit_id}")
     total = 0
     char_string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%"
@@ -20,6 +32,12 @@ def calculate_check_digit(kit_id: str) -> str:
     return f"{kit_id}-{check_digit}"
 
 def convert_kit_id_to_fit_device_id(kit_id: str) -> str:
+    """
+    This is used to add the expiry date to the end of the Kit ID
+    This by setting the month to december
+    And the year is set to 1 year in the future.
+    E.g. if the current date is 06/24 the expiry date will be set to 12/25
+    """
     logging.info(f"Generating FIT Device ID from: {kit_id}")
     today = datetime.now()
     year = today.strftime("%y")  # Get the year from todays date in YY format
--- utils/oracle/oracle_specific_functions.py
+++ utils/oracle/oracle_specific_functions.py
@@ -3,6 +3,13 @@ import logging
 import pandas as pd
 
 def get_kit_id_from_db() -> pd.DataFrame:
+    """
+    This query is used to obtain test kits used in compartment 2
+    It searches for kits that have not been logged and meet the following criteria:
+    - tk.tk_type_id = 2 (Only FIT Kits)
+    - sdc.hub_id = 23159 (Hub ID that the compartments are running in)
+    - se.latest_event_status_id is 11198 or 11213 (Only kits at the status we want S10/S19 are retrieved)
+    """
     logging.info("Retrieving useable test kit ids")
     kit_id_df = OracleDB().execute_query("""select tk.kitid, tk.screening_subject_id, sst.subject_nhs_number
     from tk_items_t tk
@@ -20,6 +27,10 @@ def get_kit_id_from_db() -> pd.DataFrame:
     return kit_id_df
 
 def get_nhs_no_from_batch_id(batch_id) -> pd.DataFrame:
+    """
+    This query returns a dataframe of NHS Numbers of the subjects in a certain batch
+    We provide the batch ID e.g. 8812 and then we have a list of NHS Numbers we can verify the statuses of
+    """
     nhs_number_df = OracleDB().execute_query(f"""
     SELECT SUBJECT_NHS_NUMBER
     FROM SCREENING_SUBJECT_T ss
