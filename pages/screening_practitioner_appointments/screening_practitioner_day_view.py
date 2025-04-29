80dc46794538ca9994166a55a9ae824bb2797aea
Megha Prasannan <megha.prasannan1@nhs.net>
2025-04-29 15:36:34 +0100

    Added a new page for screening-practitioner-day-view and refactor code in compartment5

---
+++ pages/screening_practitioner_appointments/screening_practitioner_day_view.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Page, expect
+from pages.base_page import BasePage
+from utils.table_util import TableUtils
+
+
+class ScreeningPractitionerDayView(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+        self.page = page
+        # Screening Practitioner Day View   - page locators
+        self.calendar_button = page.get_by_role("button", name="Calendar")
+
+    def click_calendar_button(self) -> None:
+        self.click(self.calendar_button)
+
+    def click_patient_link(self, patient_name: str) -> None:
+        self.click(self.page.get_by_role("link", name=patient_name))
