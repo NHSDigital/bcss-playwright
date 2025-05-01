a3d391f7882765561225731edd9a4c0085477fe4
AndyG <179915678+Andyg79@users.noreply.github.com>
2025-05-01 08:34:02 +0100

    Addressed PR comments - fix imports

--- tests/smokescreen/test_compartment_5.py
+++ tests/smokescreen/test_compartment_5.py
@@ -2,11 +2,14 @@ import pytest
 from playwright.sync_api import Page, expect
 from pages.logout.log_out_page import Logout
 from pages.base_page import BasePage
-from pages.screening_practitioner_appointments import (
+from pages.screening_practitioner_appointments.screening_practitioner_appointments import (
     ScreeningPractitionerAppointmentsPage,
-    subject_datasets,
 )
-from pages.screening_subject_search import FitForColonoscopySspOptions, AsaGradeOptions
+from pages.screening_practitioner_appointments.subject_datasets import (
+    SubjectDatasets,
+    FitForColonoscopySspOptions,
+    AsaGradeOptions,
+)
 from pages.screening_subject_search.subject_screening_summary import (
     SubjectScreeningSummary,
 )
@@ -89,23 +92,23 @@ def test_compartment_5(page: Page, smokescreen_properties: dict) -> None:
     SubjectScreeningSummary(page).click_datasets_link()
 
     # Click on 'Show Dataset' next to the Colonoscopy Assessment
-    subject_datasets(page).click_show_datasets()
+    SubjectDatasets(page).click_show_datasets()
 
     # Populate Colonoscopy Assessment Details fields
 
     # ASA Grade  - I - Fit
-    subject_datasets(page).select_asa_grade_option(AsaGradeOptions.FIT.value)
+    SubjectDatasets(page).select_asa_grade_option(AsaGradeOptions.FIT.value)
 
     # Fit for Colonoscopy (SSP) - Yes
-    subject_datasets(page).select_fit_for_colonoscopy_option(
+    SubjectDatasets(page).select_fit_for_colonoscopy_option(
         FitForColonoscopySspOptions.YES.value
     )
 
     # Click 'Yes' for Dataset Complete?
-    subject_datasets(page).click_dataset_complete_radio_button_yes()
+    SubjectDatasets(page).click_dataset_complete_radio_button_yes()
 
     # Click Save Dataset button
-    subject_datasets(page).save_dataset()
+    SubjectDatasets(page).save_dataset()
 
     # Click Back
     BasePage(page).click_back_button()
