7cceee5510cc5029d3e7b69dd408204e287769cd
AndyG <179915678+Andyg79@users.noreply.github.com>
2025-07-02 10:30:25 +0100

    wip

--- tests/regression/subject/manual_cease/test_manual_ui_unceasing_checks_existing_kit_present.py
+++ tests/regression/subject/manual_cease/test_manual_ui_unceasing_checks_existing_kit_present.py
@@ -1,8 +1,24 @@
-# @BCSSAdditionalTests
-# Feature: Manual UI Unceasing Checks - Existing Kit Present
-
-# A manually ceased subject with unlogged kits can be unceased as part of the late response "log an existing kit" process.
-
+import pytest
+from playwright.sync_api import Page
+from utils.user_tools import UserTools
+from classes.user import User
+from classes.subject import Subject
+from pages.base_page import BasePage
+from pages.screening_subject_search.subject_demographic_page import (
+    SubjectDemographicPage,
+)
+from pages.logout.log_out_page import LogoutPage
+from utils.screening_subject_page_searcher import (
+    search_subject_demographics_by_nhs_number,
+)
+from utils.oracle.oracle import OracleDB
+from utils.oracle.oracle_specific_functions import (
+    check_if_subject_has_temporary_address,
+)
+from utils.oracle.subject_selection_query_builder import SubjectSelectionQueryBuilder
+import logging
+from faker import Faker
+from datetime import datetime, timedelta
 
 # Further Reading
 # --------
@@ -10,8 +26,26 @@
 # Q:\DEV\Services\BCSS\System Specification\Screening Subject
 # The spreadsheet itself is called: Screening Subject Data Items (Use the latest version)
 
+# @BCSSAdditionalTests
+# Feature: Manual UI Unceasing Checks - Existing Kit Present
+
+# A manually ceased subject with unlogged kits can be unceased as part of the late response "log an existing kit" process.
+
 # Background: I log in as a hub manager for BCSS England for all tests
 # Given I log in to BCSS "England" as user role "Hub Manager"
+@pytest.fixture(scope="function", autouse=True)
+def before_each(page: Page) -> str:
+    """
+    Before every test is executed, this fixture:
+    - Logs into BCSS as a Screening Centre Manager at BCS001
+    - Navigates to the screening subject search page
+    """
+    nhs_no = obtain_test_data_nhs_no()
+    logging.info(f"Selected NHS Number: {nhs_no}")
+    UserTools.user_login(page, "Screening Centre Manager at BCS001")
+    BasePage(page).go_to_screening_subject_search_page()
+    search_subject_demographics_by_nhs_number(page, nhs_no)
+    return nhs_no
 
 
 # Scenario: Subject is within eligible age range, is next due to be screened in the past and has an outstanding kit > SSUN9.8 Opt-in log a kit
@@ -157,3 +191,32 @@
 # 	| 55  |
 # 	| 53  |
 # 	| 51  |
+
+
+def obtain_test_data_nhs_no() -> str:
+    """
+    Obtain a test subject's NHS number that matches the following criteria:
+    | Subject age                   | <= 80 |
+        | Subject has temporary address | No    |
+
+    This is obtained using the Subject Selection Query Builder.
+
+    Returns:
+        str: The NHS number of the subject that matches the criteria.
+    """
+    criteria = {
+        "subject age": "<= 80",
+        "subject has temporary address": "no",
+    }
+    user = User()
+    subject = Subject()
+
+    builder = SubjectSelectionQueryBuilder()
+
+    query, bind_vars = builder.build_subject_selection_query(
+        criteria=criteria, user=user, subject=subject, subjects_to_retrieve=1
+    )
+
+    df = OracleDB().execute_query(query, bind_vars)
+    nhs_no = df.iloc[0]["subject_nhs_number"]
+    return nhs_no
