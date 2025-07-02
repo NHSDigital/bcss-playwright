daa5dc28b90b19984622c290625058729e86114e
vidhya-chandra1 <198918051+vidhya-chandra1@users.noreply.github.com>
2025-07-02 16:49:34 +0100

    As per PR # 98 comments, Code change is implemented.

--- pages/organisations/organisations_page.py
+++ pages/organisations/organisations_page.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page
 from pages.base_page import BasePage
-
+from typing import List
 
 class OrganisationsPage(BasePage):
     """Organisations Page locators, and methods for interacting with the page."""
@@ -51,36 +51,47 @@ class OrganisationsPage(BasePage):
         """Clicks the 'Bureau' link."""
         self.click(self.bureau_page)
 
+class OrganisationNotSelectedError(Exception):
+    """Raised when no organisation is selected on the organisation switch page."""
+    pass
 
 class OrganisationSwitchPage:
-    """Organisation Switch Page locators and methods for interacting with the page"""
+    """Organisation Switch Page locators and reusable interactions"""
+
+    SELECT_ORG_LINK_TEXT = "Select Org"
 
     def __init__(self, page: Page):
         self.page = page
-        self.radio_selector = "input[name='organisation']"
-        self.select_org_link = "a:has-text('Select Organisation')"
 
-    RADIO_SELECTOR = "input[type='radio']"
-    SELECT_ORG_LINK_TEXT = "Select Org"
-    LOGIN_INFO_SELECTOR = "td.loginInfo"
+        # Locators initialized using Playwright's locator API
+        self.radio_buttons = self.page.locator("input[type='radio']")
+        self.selected_radio = self.page.locator("input[name='organisation']:checked")
+        self.continue_button = self.page.get_by_role("button", name="Continue")
+        self.select_org_link = self.page.get_by_role("link", name=self.SELECT_ORG_LINK_TEXT)
+        self.login_info = self.page.locator("td.loginInfo")
 
-    def get_available_organisation_ids(self) -> list[str]:
-        radios = self.page.locator(self.RADIO_SELECTOR)
+    def click(self, locator) -> None:
+        locator.click()
+
+    def get_available_organisation_ids(self) -> List[str]:
         org_ids = []
-        for int in range(radios.count()):
-            org_id = radios.nth(int).get_attribute("id")
+        count = self.radio_buttons.count()
+        for element in range(count):
+            org_id = self.radio_buttons.nth(element).get_attribute("id")
             if org_id:
                 org_ids.append(org_id)
         return org_ids
 
     def select_organisation_by_id(self, org_id: str) -> None:
-        self.page.locator(f"#{org_id}").check()
+        self.click(self.page.locator(f"#{org_id}"))
 
     def click_continue(self) -> None:
-        self.page.get_by_role("button", name="Continue").click()
+        self.click(self.continue_button)
 
     def click_select_org_link(self) -> None:
-        self.page.get_by_role("link", name=self.SELECT_ORG_LINK_TEXT).click()
+        self.click(self.select_org_link)
 
     def get_logged_in_text(self) -> str:
-        return self.page.locator(self.LOGIN_INFO_SELECTOR).inner_text()
+        return self.login_info.inner_text()
+
+
