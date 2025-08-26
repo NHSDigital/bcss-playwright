96f2b5e331666716516b49b7f6ebae3df22c7a07
vidhya-chandra1 <198918051+vidhya-chandra1@users.noreply.github.com>
2025-08-26 20:25:37 +0100

    As per PR # 123 comments code change is implemented.

--- pages/gfobt_test_kits/gfobt_test_kits_page.py
+++ pages/gfobt_test_kits/gfobt_test_kits_page.py
@@ -8,9 +8,7 @@ class GFOBTTestKitsPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
         self.page = page
-        self.test_kits_link = self.page.get_by_text("gFOBT Test Kits")
         self.test_kits_header = self.page.get_by_text("gFOBT Test Kits")
-
         self.test_kit_logging_page = self.page.get_by_role(
             "link", name="Test Kit Logging"
         )
@@ -39,6 +37,6 @@ class GFOBTTestKitsPage(BasePage):
         self.click(self.create_qc_kit_page)
 
     def open_test_kits_report(self):
-        self.test_kits_link.click()
+        self.click(self.test_kits_header)
 
 
