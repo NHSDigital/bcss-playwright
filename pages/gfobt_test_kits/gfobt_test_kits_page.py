2d4715f723a45d9a067122b5bc1c7dd5392b0407
vidhya-chandra1 <198918051+vidhya-chandra1@users.noreply.github.com>
2025-08-28 12:59:47 +0100

    As per PR # 123 review comments, code change is implemented.

--- pages/gfobt_test_kits/gfobt_test_kits_page.py
+++ pages/gfobt_test_kits/gfobt_test_kits_page.py
@@ -37,6 +37,7 @@ class GFOBTTestKitsPage(BasePage):
         self.click(self.create_qc_kit_page)
 
     def open_test_kits_report(self):
+        """Clicks the 'Test Kits' header to open the corresponding report section."""
         self.click(self.test_kits_header)
 
 
