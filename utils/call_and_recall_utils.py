21fd80c2d7aedb7178730b296085f7b1527155ef
Adriano Aru <adriano.aru1@nhs.net>
2025-09-02 09:07:15 +0100

    Adding run_failsafe method

---
+++ utils/call_and_recall_utils.py
@@ -0,0 +1,80 @@
+import logging
+import oracledb
+from utils.oracle.oracle import OracleDB
+from classes.repositories.general_repository import GeneralRepository
+from classes.repositories.database_transition_parameters import (
+    DatabaseTransitionParameters,
+)
+
+
+class CallAndRecallUtils:
+    """
+    This contains utility methods to do with Call and Recall
+    """
+
+    def __init__(self):
+        self.oracledb = OracleDB()
+
+    def run_failsafe(self, nhs_no: str) -> None:
+        """
+        Run the failsafe trawl for the given NHS number.
+        Args:
+            nhs_no: The NHS number of the subject
+        """
+        subject_id = int(self.oracledb.get_subject_id_from_nhs_number(nhs_no))
+        conn = self.oracledb.connect_to_db()
+        conn.callTimeout = 30000  # Setting call timeout to 30 seconds
+        cur = conn.cursor()
+
+        pi = cur.var(oracledb.NUMBER)
+        pi.setvalue(0, subject_id)
+
+        out_cursor = cur.var(oracledb.CURSOR)
+
+        cur.execute(
+            """
+            BEGIN
+                pkg_fobt_call.p_failsafe_trawl(
+                    pi_subject_id => :pi,
+                    po_cur_error  => :po
+                );
+            END;""",
+            {"pi": str(subject_id), "po": out_cursor},
+        )
+
+        result_cursor = out_cursor.getvalue()
+        row = result_cursor.fetchone()
+        assert (
+            "The action was performed successfully" in row
+        ), f"Error when executing failsafe for {nhs_no}: {row}"
+
+        # Clean up
+        result_cursor.close()
+        cur.close()
+        conn.close()
+        logging.info(f"END: failsafe stored proc executed for {nhs_no}")
+
+    def invite_subject_for_fobt_screening(self, nhs_no: str) -> None:
+        """
+        Runs the database transition to 'invite' the subject for FOBT screening and create an FOBT episode.
+        Uses OracleDB to execute the stored procedure.
+        Args:
+            nhs_no: The NHS number of the subject
+        """
+        logging.info(f"START: invite_subject_for_fobt_screening for NHS No: {nhs_no}")
+
+        try:
+            # Prepare parameters for the stored procedure
+            general_repository = GeneralRepository()
+            database_transition_parameters = DatabaseTransitionParameters(
+                transition_id=58,
+                subject_id=int(self.oracledb.get_subject_id_from_nhs_number(nhs_no)),
+                user_id=2,
+                rollback_on_failure="Y",
+            )
+            general_repository.run_database_transition(database_transition_parameters)
+        except Exception as e:
+            raise Exception(
+                f"Error in invite_subject_for_fobt_screening for NHS No {nhs_no}: {e}"
+            ) from e
+        logging.info(f"END: invite_subject_for_fobt_screening for NHS No: {nhs_no}")
