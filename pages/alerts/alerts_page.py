53c20fa63709cd222fa1bdedc134203ae8f85e01
vidhya-chandra1 <198918051+vidhya-chandra1@users.noreply.github.com>
2025-09-09 18:27:15 +0100

    As per PR # 123 comments, code change is implemented.

--- pages/alerts/alerts_page.py
+++ pages/alerts/alerts_page.py
@@ -7,11 +7,11 @@ class AlertsPage:
         self.page = page
         self.refresh_alerts_link = self.page.get_by_role("link", name="Refresh alerts")
 
-    def click_refresh_alerts(self):
+    def click_refresh_alerts(self) -> None:
         """Clicks the 'Refresh Alerts' link to trigger an update of alert messages."""
         self.refresh_alerts_link.click()
 
-    def is_refresh_alerts_visible(self, timeout=5000):
+    def is_refresh_alerts_visible(self, timeout=5000) -> bool:
         """Returns True if the 'Refresh Alerts' link is visible within the given timeout."""
         return self.refresh_alerts_link.is_visible(timeout=timeout)
 
