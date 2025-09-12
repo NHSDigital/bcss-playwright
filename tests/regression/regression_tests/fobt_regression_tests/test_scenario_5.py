d075be5134cd489ae8abb846e731a2a3b4f0e693
AndyG <179915678+Andyg79@users.noreply.github.com>
2025-09-12 17:16:49 +0100

    wip & added mark_appointment_as_dna util to utils/appointments

--- tests/regression/regression_tests/fobt_regression_tests/test_scenario_5.py
+++ tests/regression/regression_tests/fobt_regression_tests/test_scenario_5.py
@@ -13,10 +13,9 @@ from utils.fit_kit import FitKitLogged, FitKitGeneration
 from pages.screening_subject_search.subject_screening_summary_page import (
     SubjectScreeningSummaryPage,
 )
-
-# from pages.screening_subject_search.close_fobt_screening_episode_page import (
-#     CloseFobtScreeningEpisodePage,
-# )
+from pages.screening_subject_search.close_fobt_screening_episode_page import (
+    CloseFobtScreeningEpisodePage,
+)
 from utils.appointments import book_appointments
 from pages.logout.log_out_page import LogoutPage
 from pages.base_page import BasePage
@@ -33,6 +32,7 @@ from pages.screening_subject_search.advance_fobt_screening_episode_page import (
 from pages.screening_subject_search.record_diagnosis_date_page import (
     RecordDiagnosisDatePage,
 )
+from utils.appointments import mark_appointment_as_dna
 
 
 @pytest.mark.wip
@@ -142,153 +142,245 @@ def test_scenario_5(page: Page) -> None:
 
     # Then there is a "S1" letter batch for my subject with the exact title "Pre-invitation (FIT)"
     # When I process the open "S1" letter batch for my subject
+    # Then my subject has been updated as follows:
+    batch_processing(
+        page,
+        "S1",
+        "Pre-invitation (FIT)",
+        "S9 - Pre-invitation Sent",
+        True,
+    )
+
     # When I run Timed Events for my subject
     # Then there is a "S9" letter batch for my subject with the exact title "Invitation & Test Kit (FIT)"
+    # When I process the open "S9" letter batch for my subject
+    # # Then my subject has been updated as follows:
+    batch_processing(
+        page,
+        "S9",
+        "Invitation & Test Kit (FIT)",
+        "S10 - Invitation & Test Kit Sent",
+        True,
+    )
+
+    # When I log my subject's latest unlogged FIT kit
+    fit_kit = FitKitGeneration().get_fit_kit_for_subject_sql(nhs_no, False, False)
+    sample_date = datetime.now()
+    FitKitLogged().log_fit_kits(page, fit_kit, sample_date)
+
     # Then my subject has been updated as follows:
-    # batch_processing( TODO: Check these values
-    #     page,
-    #     "S1",
-    #     "Pre-invitation (FIT)",
-    #     "S9 - Invitation & Test Kit (FIT)",
-    #     True,
-    # )
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "S43 Kit Returned and Logged (Initial Test)",
+        },
+    )
 
+    # When I read my subject's latest logged FIT kit as "ABNORMAL"
+    FitKitLogged().read_latest_logged_kit(user_role, 2, fit_kit, "ABNORMAL")
 
-# When I process the open "S9" letter batch for my subject
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "A8 Abnormal",
+        },
+    )
 
-# Latest event status	S10 Invitation & Test Kit Sent
+    # When I view the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
 
-# When I log my subject's latest unlogged FIT kit
-# And I pause for "6" seconds to let the process complete
-# Then my subject has been updated as follows:
+    # And I choose to book a practitioner clinic for my subject
+    SubjectScreeningSummaryPage(page).click_book_practitioner_clinic_button()
 
-# Latest event status	S43 Kit Returned and Logged (Initial Test)
+    # And I select "BCS001" as the screening centre where the practitioner appointment will be held
+    # And I set the practitioner appointment date to "today"
+    # And I book the "earliest" available practitioner appointment on this date
+    book_appointments(
+        page,
+        "BCS001 - Wolverhampton Bowel Cancer Screening Centre",
+        "The Royal Hospital (Wolverhampton)",
+    )
 
-# When I read my subject's latest logged FIT kit as "ABNORMAL"
-# Then my subject has been updated as follows:
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "A183 1st Colonoscopy Assessment Appointment Requested",
+        },
+    )
 
-# Latest event status	A8 Abnormal
+    # And there is a "A183" letter batch for my subject with the exact title "Practitioner Clinic 1st Appointment"
+    # When I process the open "A183 - Practitioner Clinic 1st Appointment" letter batch for my subject
+    # Then my subject has been updated as follows:
+    batch_processing(
+        page,
+        "A183",
+        "Practitioner Clinic 1st Appointment",
+        "A25 - 1st Colonoscopy Assessment Appointment Booked, letter sent",
+    )
 
-# When I view the subject
-# And I choose to book a practitioner clinic for my subject
-# And I select "BCS001" as the screening centre where the practitioner appointment will be held
-# And I set the practitioner appointment date to "today"
-# And I book the "earliest" available practitioner appointment on this date
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # TODO: And there is a "A183" letter batch for my subject with the exact title "GP Result (Abnormal)"
 
-# Latest event status	A183 1st Colonoscopy Assessment Appointment Requested
-# And there is a "A183" letter batch for my subject with the exact title "Practitioner Clinic 1st Appointment"
-# And there is a "A183" letter batch for my subject with the exact title "GP Result (Abnormal)"
+    # When I switch users to BCSS "England" as user role "Screening Centre Manager"
+    LogoutPage(page).log_out(close_page=False)
+    BasePage(page).go_to_log_in_page()
+    UserTools.user_login(page, "Screening Centre Manager at BCS001")
 
-# When I process the open "A183 - Practitioner Clinic 1st Appointment" letter batch for my subject
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # And I view the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
 
-# Latest event status	A25 1st Colonoscopy Assessment Appointment Booked, letter sent
+    # And I view the event history for the subject's latest episode
+    # And I view the latest practitioner appointment in the subject's episode
+    # And The subject DNAs the practitioner appointment
+    mark_appointment_as_dna(page, "Patient did not attend")
 
-# When I switch users to BCSS "England" as user role "Screening Centre Manager"
-# And I view the subject
-# And I view the event history for the subject's latest episode
-# And I view the latest practitioner appointment in the subject's episode
-# And The subject DNAs the practitioner appointment
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # And there is a "J11" letter batch for my subject with the exact title "Practitioner Clinic 1st Appointment Non Attendance (Patient)"
+    # When I process the open "J11" letter batch for my subject
+    # Then my subject has been updated as follows:
+    batch_processing(
+        page,
+        "J11",
+        "Practitioner Clinic 1st Appointment Non Attendance (Patient)",
+        "J27 Appointment Non-attendance Letter Sent (Patient)",
+    )
 
-# Latest event status	J11 1st Colonoscopy Assessment Appointment Non-attendance (Patient)
-# And there is a "J11" letter batch for my subject with the exact title "Practitioner Clinic 1st Appointment Non Attendance (Patient)"
+    # When I view the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
 
-# When I process the open "J11" letter batch for my subject
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # And I choose to book a practitioner clinic for my subject
+    SubjectScreeningSummaryPage(page).click_book_practitioner_clinic_button()
 
-# Latest event status	J27 Appointment Non-attendance Letter Sent (Patient)
+    # And I select "BCS001" as the screening centre where the practitioner appointment will be held
+    # And I set the practitioner appointment date to "today"
+    # And I book the "earliest" available practitioner appointment on this date
+    book_appointments(
+        page,
+        "BCS001 - Wolverhampton Bowel Cancer Screening Centre",
+        "The Royal Hospital (Wolverhampton)",
+    )
 
-# When I view the subject
-# And I choose to book a practitioner clinic for my subject
-# And I select "BCS001" as the screening centre where the practitioner appointment will be held
-# And I set the practitioner appointment date to "today"
-# And I book the "earliest" available practitioner appointment on this date
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "A184 2nd Colonoscopy Assessment Appointment Requested",
+        },
+    )
 
-# Latest event status	A184 2nd Colonoscopy Assessment Appointment Requested
-# And there is a "A184" letter batch for my subject with the exact title "Practitioner Clinic 2nd Appointment"
+    # And there is a "A184" letter batch for my subject with the exact title "Practitioner Clinic 2nd Appointment"
+    # When I process the open "A184 - Practitioner Clinic 2nd Appointment" letter batch for my subject
+    # Then my subject has been updated as follows:
+    batch_processing(
+        page,
+        "A184",
+        "Practitioner Clinic 2nd Appointment",
+        "A26 - 2nd Colonoscopy Assessment Appointment Booked, letter sent",
+    )
 
-# When I process the open "A184 - Practitioner Clinic 2nd Appointment" letter batch for my subject
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # When I view the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
 
-# Latest event status	A26 2nd Colonoscopy Assessment Appointment Booked, letter sent
+    # And I view the event history for the subject's latest episode
+    # And I view the latest practitioner appointment in the subject's episode
+    # And The subject DNAs the practitioner appointment
+    mark_appointment_as_dna(page, "Patient did not attend")
 
-# When I view the subject
-# And I view the event history for the subject's latest episode
-# And I view the latest practitioner appointment in the subject's episode
-# And The subject DNAs the practitioner appointment
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest event status": "A185 2nd Colonoscopy Assessment Appointment Non-attendance (Patient)",
+        },
+    )
 
-# Latest event status	A185 2nd Colonoscopy Assessment Appointment Non-attendance (Patient)
-# And there is a "A185" letter batch for my subject with the exact title "Patient Discharge (Non Attendance of Practitioner Clinic)"
+    # And there is a "A185" letter batch for my subject with the exact title "Patient Discharge (Non Attendance of Practitioner Clinic)"
+    # When I process the open "A185" letter batch for my subject
+    # Then my subject has been updated as follows:
+    batch_processing(
+        page,
+        "A185",
+        "Patient Discharge (Non Attendance of Practitioner Clinic)",
+        "A37 Patient Discharge Sent (Non-attendance at Colonoscopy Assessment Appointment)",
+    )
 
-# When I process the open "A185" letter batch for my subject
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # When I switch users to BCSS "England" as user role "Hub Manager"
+    LogoutPage(page).log_out(close_page=False)
+    BasePage(page).go_to_log_in_page()
+    UserTools.user_login(page, "Hub Manager State Registered at BCS01")
 
-# Latest event status	A37 Patient Discharge Sent (Non-attendance at Colonoscopy Assessment Appointment)
-# And there is a "A37" letter batch for my subject with the exact title "GP Discharge (Non Attendance of Practitioner Clinic)"
+    # And there is a "A37" letter batch for my subject with the exact title "GP Discharge (Non Attendance of Practitioner Clinic)"
+    # And I process the open "A37" letter batch for my subject
+    # Then my subject has been updated as follows:
+    batch_processing(
+        page,
+        "A37",
+        "GP Discharge (Non Attendance of Practitioner Clinic)",
+        "P202 Waiting Completion of Outstanding Events",
+    )
 
-# When I switch users to BCSS "England" as user role "Hub Manager"
-# And I process the open "A37" letter batch for my subject
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # When I view the subject
+    screening_subject_page_searcher.navigate_to_subject_summary_page(page, nhs_no)
 
-# Latest event status	P202 Waiting Completion of Outstanding Events
+    # And I view the advance episode options
+    SubjectScreeningSummaryPage(page).click_advance_fobt_screening_episode_button()
 
-# When I view the subject
-# And I view the advance episode options
-# And I select the advance episode option for "Record Diagnosis Date"
-# And I enter a Diagnosis Date of "today"
-# And I save Diagnosis Date Information
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # And I select the advance episode option for "Record Diagnosis Date"
+    AdvanceFOBTScreeningEpisodePage(page).click_record_diagnosis_date_button()
 
-# Latest episode diagnosis date reason	Null
-# Latest episode has diagnosis date   	Yes
-# Latest episode includes event status	A50 Diagnosis date recorded
-# Latest event status                 	P202 Waiting Completion of Outstanding Events
+    # And I enter a Diagnosis Date of "today"
+    RecordDiagnosisDatePage(page).enter_date_in_diagnosis_date_field(datetime.now())
 
-# When I process the open "A183 - GP Result (Abnormal)" letter batch for my subject
-# And I pause for 5 seconds to let the process complete
-# Then my subject has been updated as follows:
+    # And I save Diagnosis Date Information
+    RecordDiagnosisDatePage(page).click_save_button()
 
-# Calculated FOBT due date                 	2 years from latest A37 event
-# Calculated lynch due date               	Unchanged
-# Calculated surveillance due date         	Unchanged
-# Ceased confirmation date                 	Null
-# Ceased confirmation details             	Null
-# Ceased confirmation user ID             	Null
-# Clinical reason for cease               	Null
-# Latest episode accumulated result       	Definitive abnormal FOBT outcome
-# Latest episode includes event status     	A167 GP Abnormal FOBT Result Sent
-# Latest episode recall calculation method	Date of last patient letter
-# Latest episode recall episode type       	FOBT Screening
-# Latest episode recall surveillance type 	Null
-# Latest episode status                   	Closed
-# Latest episode status reason             	Non Response
-# Latest event status                     	A166 GP Discharge Sent (No show for Colonoscopy Assessment Appointment)
-# Lynch due date                           	Null
-# Lynch due date date of change           	Unchanged
-# Lynch due date reason                   	Unchanged
-# Screening due date                       	Calculated FOBT due date
-# Screening due date date of change       	Today
-# Screening due date reason               	Recall
-# Screening status                         	Recall
-# Screening status date of change
-# Not checking as status may or may not have changed
-# Screening status reason                 	Recall
-# Surveillance due date                   	Null
-# Surveillance due date date of change     	Unchanged
-# Surveillance due date reason             	Unchanged
+    # Then my subject has been updated as follows:
+    subject_assertion(
+        nhs_no,
+        {
+            "latest episode diagnosis date reason": "Null",
+            "latest episode has diagnosis date": "Yes",
+            "latest episode includes event status": "A50 Diagnosis date recorded",
+            "latest event status": "P202 Waiting Completion of Outstanding Events",
+        },
+    )
+
+    # When I process the open "A183 - GP Result (Abnormal)" letter batch for my subject
+    # Then my subject has been updated as follows:
+    batch_processing(
+        page,
+        "A183",
+        "GP Result (Abnormal)",
+        "A166 GP Discharge Sent (No show for Colonoscopy Assessment Appointment)",
+    )
+
+    subject_assertion(
+        nhs_no,
+        {
+            "calculated FOBT due date": "2 years from latest A37 event",
+            "calculated lynch due date": "Unchanged",
+            "calculated surveillance due date": "Unchanged",
+            "ceased confirmation date": "Null",
+            "ceased confirmation details": "Null",
+            "ceased confirmation user ID": "Null",
+            "clinical reason for cease": "Null",
+            "latest episode accumulated result": "Definitive abnormal FOBT outcome",
+            "latest episode includes event status": "A167 GP Abnormal FOBT Result Sent",
+            "latest episode recall calculation method": "Date of last patient letter",
+            "latest episode recall episode type": "FOBT Screening",
+            "latest episode recall surveillance type": "Null",
+            "latest episode status": "Closed",
+            "latest episode status reason": "Non Response",
+            "lynch due date": "Null",
+            "lynch due date date of change": "Unchanged",
+            "lynch due date reason": "Unchanged",
+            "screening due date": "Calculated FOBT due date",
+            "screening due date date of change": "Today",
+            "screening due date reason": "Recall",
+            "screening status": "Recall",
+            "screening status reason": "Recall",
+            "surveillance due date": "Null",
+            "surveillance due date date of change": "Unchanged",
+            "surveillance due date reason": "Unchanged",
+        },
+    )
