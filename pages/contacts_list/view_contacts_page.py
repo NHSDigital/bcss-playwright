bba1d60df9c9a823c915970069c55c8ab6651429
Hilary Martin <hilary.martin@nhs.net>
2025-12-08 11:30:11 +0000

    updated view contacts page

--- pages/contacts_list/view_contacts_page.py
+++ pages/contacts_list/view_contacts_page.py
@@ -1,4 +1,5 @@
-from playwright.sync_api import Page, expect
+import logging
+from playwright.sync_api import Page
 from pages.base_page import BasePage
 
 
@@ -12,6 +13,15 @@ class ViewContactsPage(BasePage):
 
     def verify_view_contacts_title(self) -> None:
         """Verify the View Contacts page title is displayed correctly"""
-        self.bowel_cancer_screening_page_title_contains_text(
-            "View Contacts"
-        )
+        self.bowel_cancer_screening_page_title_contains_text("View Contacts")
+
+    def search_by_job_role_and_organisation_code(
+        self, job_role: str, org_code: str
+    ) -> None:
+        """
+        search by provided job role and organisation
+        """
+        self.page.locator('input[name="selJobRole"]').fill(job_role)
+        self.page.locator('input[name="selOrganisationCode"]').fill(org_code)
+        self.page.get_by_role("button", name="Search").click()
+        logging.info(f"input {job_role} and {org_code} and click on search")
