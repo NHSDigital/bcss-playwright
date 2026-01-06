eb74d952115981482cbf8adb8b902587ec214c67
Adriano Aru <adriano.aru1@nhs.net>
2026-01-06 12:27:24 +0000

    Minor refactoring

--- pages/screening_subject_search/advance_lynch_surveillance_episode_page.py
+++ pages/screening_subject_search/advance_lynch_surveillance_episode_page.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from playwright.sync_api import Page
 from pages.screening_subject_search.advance_episode_page import AdvanceEpisodePage
+from utils.calendar_picker import CalendarPicker
 
 
 class AdvanceLynchSurveillanceEpisodePage(AdvanceEpisodePage):
@@ -10,24 +12,29 @@ class AdvanceLynchSurveillanceEpisodePage(AdvanceEpisodePage):
         self.page = page
 
         # Locators
-        self.review_suitability_for_lynch_surveillance_button = page.get_by_role(
+        self.review_suitability_for_lynch_surveillance_button = self.page.get_by_role(
             "button", name="Review suitability for Lynch Surveillance"
         )
-        self.refer_for_clinician_review_button = page.get_by_role(
+        self.refer_for_clinician_review_button = self.page.get_by_role(
             "button", name="Refer for Clinician Review"
         )
         self.close_lynch_surveillance_episode_incorrect_diagnosis_button = (
-            page.get_by_role(
+            self.page.get_by_role(
                 "button", name="Close Lynch Surveillance Episode (Incorrect Diagnosis)"
             )
         )
-        self.close_lynch_surveillance_episode_clinical_reason_button = page.get_by_role(
-            "button", name="Close Lynch Surveillance Episode (Clinical Reason)"
+        self.close_lynch_surveillance_episode_clinical_reason_button = (
+            self.page.get_by_role(
+                "button", name="Close Lynch Surveillance Episode (Clinical Reason)"
+            )
         )
         self.close_lynch_surveillance_episode_recent_colonsocopy_button = (
-            page.get_by_role(
+            self.page.get_by_role(
                 "button", name="Close Lynch Surveillance Episode (Recent Colonoscopy)"
             )
+        )
+        self.last_colonoscopy_date_field = self.page.locator(
+            "input[type='text'][id^='UI_APPT_DATE_']"
         )
 
     def click_review_suitability_for_lynch_surveillance_button(self) -> None:
@@ -55,4 +62,14 @@ class AdvanceLynchSurveillanceEpisodePage(AdvanceEpisodePage):
         """Click on the 'Close Lynch Surveillance Episode (Recent Colonoscopy)' button."""
         self.safe_accept_dialog(
             self.close_lynch_surveillance_episode_recent_colonsocopy_button
+        )
+
+    def enter_lynch_last_colonoscopy_date(self, date: datetime) -> None:
+        """Enter the Lynch Last Colonoscopy Date.
+
+        Args:
+            date (datetime): The date to enter.
+        """
+        CalendarPicker(self.page).calendar_picker_ddmmyyyy(
+            date, self.last_colonoscopy_date_field
         )
